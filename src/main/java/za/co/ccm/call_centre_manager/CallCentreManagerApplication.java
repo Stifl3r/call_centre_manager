0553a695d5ef3ecf4d09cf2a48f4ef42b1c1dfb7
Arnold <arnold.madisa@momentum.co.za>
2020-11-08 18:23:24 +0200

    [Arnold] Scaffold springboot app

---
+++ src/main/java/za/co/ccm/call_centre_manager/CallCentreManagerApplication.java
@@ -0,0 +1,13 @@
+package za.co.ccm.call_centre_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCentreManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCentreManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/ccm/call_centre_manager/CallCentreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.ccm.call_centre_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCentreManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
